0649f52a92ef1bd2761f40a1d067015e5eca65d0
Douglas Scaini <douglasscaini.dev@gmail.com>
2023-09-01 17:29:12 -0300

    add initial structure and initial sidebar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,20 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: [
+        './src/**/*.tsx',
+    ],
+    theme: {
+        extend: {
+            gridTemplateColumns: {
+                app: 'minmax(18rem, 20rem) 1fr'
+            },
+            colors: {
+                rocket: '#8257e6'
+            }
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
+
 export default config

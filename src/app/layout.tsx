3c6d2d12bc66b1c6b240c4c46e0b68f5dc24879c
Douglas Scaini <douglasscaini.dev@gmail.com>
2023-11-27 20:47:38 -0300

    create abs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
-        <html lang="en">
+        <html lang="en" className="antialiased">
             <body className={inter.className}>
                 <div className="min-h-screen grid grid-cols-app">
                     <Sidebar />
